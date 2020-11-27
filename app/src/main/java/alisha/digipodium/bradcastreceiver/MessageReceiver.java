7567606f3750f7f228218168c2b00d2c85d78a0d
Alisha Singh <63709954+Alisha-creator@users.noreply.github.com>
2020-11-27 17:12:20 +0530

    Initial commit

---
+++ app/src/main/java/alisha/digipodium/bradcastreceiver/MessageReceiver.java
@@ -0,0 +1,17 @@
+package alisha.digipodium.bradcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MessageReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /*// TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");*/
+        Intent appLaunch = new Intent(context,MainActivity.class);
+        context.startActivity(appLaunch);
+    }
+}
